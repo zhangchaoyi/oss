7fe8f2e0fad8d86d0c5add0548bd3489f11dccc4
chris <zhangchaoyi@wilddog.com>
2016-09-08 21:31:01 +0800

    add info

---
+++ src/main/webapp/js/info.js
@@ -0,0 +1,36 @@
+function checkTime(i){
+	if (i<10){
+	  i="0" + i
+	}
+  	return i
+}
+setInterval(function() {
+    var date = new Date();
+    $('#current-time').text(checkTime(date.getHours()) +":"+ checkTime(date.getMinutes()) +":"+ checkTime(date.getSeconds()));
+}, 1000);
+
+//explain up and down button 
+$("#btn-explain-up").click(function(){
+    $("div.explain-content-box").css("margin-top", function(index,value){
+        value = parseFloat(value) + 155;
+        if(value>=0){
+            $("#btn-explain-up").addClass("disabled");
+        }
+        if($("#btn-explain-down").hasClass("disabled")){ 
+            $("#btn-explain-down ").removeClass("disabled");
+        }
+        return value;
+    });       
+});
+$("#btn-explain-down").click(function(){
+    $("div.explain-content-box").css("margin-top", function(index,value){
+        value = parseFloat(value) - 155;
+        if(value <= -310){
+            $("#btn-explain-down").addClass("disabled");
+        }
+        if($("#btn-explain-up").hasClass("disabled")){ 
+            $("#btn-explain-up").removeClass("disabled");
+        }
+        return value;
+    });
+});
