2e3c14480d6064193d0f94260bd063f0b14b3859
chris <zhangchaoyi@wilddog.com>
2016-09-14 11:02:06 +0800

    backup

---
+++ src/main/webapp/js/payment.js
@@ -0,0 +1,25 @@
+//explain up and down button 
+$("#btn-explain-up").click(function(){
+    $("div.explain-content-box").css("margin-top", function(index,value){
+        value = parseFloat(value) + 155;
+        if(value>=0){
+            $("#btn-explain-up").addClass("disabled");
+        }
+        if($("#btn-explain-down").hasClass("disabled")){ 
+            $("#btn-explain-down ").removeClass("disabled");
+        }
+        return value;
+    });       
+});
+$("#btn-explain-down").click(function(){
+    $("div.explain-content-box").css("margin-top", function(index,value){
+        value = parseFloat(value) - 155;
+        if(value <= -310){
+            $("#btn-explain-down").addClass("disabled");
+        }
+        if($("#btn-explain-up").hasClass("disabled")){ 
+            $("#btn-explain-up").removeClass("disabled");
+        }
+        return value;
+    });
+});
