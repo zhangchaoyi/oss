60031a4a03f678d7636defb90126fe5ffda3f0d4
chris <zhangchaoyi@wilddog.com>
2017-01-13 18:47:46 +0800

    fix null value

--- src/main/java/common/utils/StringUtils.java
+++ src/main/java/common/utils/StringUtils.java
@@ -1,8 +1,15 @@
 package common.utils;
 
+import org.apache.log4j.Logger;
+
 public class StringUtils {
+	private static Logger logger = Logger.getLogger(StringUtils.class);
 	//用于拼接sql   --icon  --account
 	public static String arrayToQueryString(String[] array){
+		if(array==null){
+			logger.info("array is null");
+			return "";
+		}
 		String str = "";
 		for(int i=0;i<array.length;i++){
 			if(i+1==array.length){
@@ -20,6 +27,6 @@ public class StringUtils {
 	
 	public static void main(String args[]){
 		String[] aa= {"a","b","c","e","d"};
-		System.out.println(arrayToQueryString(aa));
+		System.out.println("----"+arrayToQueryString(aa));
 	}
 }
